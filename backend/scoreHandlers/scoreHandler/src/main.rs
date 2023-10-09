9195a4b5c41d4d7c9be2c74850e3d7c418ac0fde
Daktic <g@eye-catcher.com>
2023-10-09 16:26:35 -0400

    Added lambda function to handle Score requests

--- backend/scoreHandlers/scoreHandler/src/main.rs
+++ backend/scoreHandlers/scoreHandler/src/main.rs
@@ -1,23 +1,24 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+mod scores;
+
+
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response, IntoResponse};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
+
+    let table_name = "scores_sep_11155111_88";
+    //let table_name = "healthbot_80001_1";
+    let scores = scores::get_scores(table_name).await?;
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
+        .header("content-type", "application/json")
+        .body(scores.to_string())
         .map_err(Box::new)?;
     Ok(resp)
 }
