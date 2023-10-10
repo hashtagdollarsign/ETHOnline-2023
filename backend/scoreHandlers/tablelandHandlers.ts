ebb2802a6f7ef1365a1be494b3c0bbb0afee05e0
Daktic <g@eye-catcher.com>
2023-10-09 20:47:19 -0400

    Using TS instead of Rust

--- backend/scoreHandlers/tablelandHandlers.ts
+++ backend/scoreHandlers/tablelandHandlers.ts
@@ -1,4 +1,18 @@
 import { Database } from "@tableland/sdk";
+// import { Signer } from "ethers";
+
+declare const window: any;
+
+// async function connectSigner(): Promise<Signer> {
+//     // Establish a connection with the browser wallet's provider.
+//     const provider = new window.ethereum;
+//     // Request the connected accounts, prompting a browser wallet popup to connect.
+//     await provider.send("eth_requestAccounts", []);
+//     // Create a signer from the returned provider connection.
+//     const signer = provider.getSigner();
+//     // Return the signer
+//     return signer;
+// }
 
 interface Scores {
     id: number;
@@ -8,6 +22,12 @@ interface Scores {
 
 const db: Database<Scores> = new Database();
 
+const getNextId = async (tableName: string) => {
+    const {id} = await db.prepare(`SELECT MAX(ID) FROM ${tableName};`).first()
+    return id + 1;
+}
+
+
 /**
  * Takes a tableName and returns an array of Scores.
  *
@@ -16,6 +36,13 @@ const db: Database<Scores> = new Database();
 const getScores = async (tableName: string) => {
     const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
     return results;
+}
+
+const postScore = async (tableName: string) => {
+    const nextId = getNextId(tableName);
+    const {meta: insert} = await db.prepare('INSERT INTO ${tableName} (id, address, score) VALUES (?,?,?);')
+        .bind(nextId, )
+        .run();
 }
 
 export {getScores}
